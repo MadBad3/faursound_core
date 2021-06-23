6b09d0d8c87431aaf8c9a58d845a2b9aa5405b95
WangCHEN9 <solid.chen9@gmail.com>
2021-06-23 11:44:15 +0200

    add metadata

--- azure_client.py
+++ azure_client.py
@@ -41,18 +41,27 @@ class fsAzureStorage(object):
         return container_client
 
 
-    def _upload_file(self,container_name, file_path):
+    def _upload_file(self,container_name, file_path, metadata = None):
 
         container_client = self._creat_or_get_container_client(container_name)
+
         with open(file_path, "rb") as data:
-            blob_client = container_client.upload_blob(name=os.path.basename(file_path), data=data)
+            blob_client = container_client.upload_blob(name=os.path.basename(file_path), data=data, 
+                                                        metadata = metadata, overwrite = True)
 
 
     def upload_to_azure(self,cv_pic_path, txt_path, predict_pic_path):
+        #? to think about how to update metadata !
+        metadata = {
+            'labeled': 'no',
+            'validated': 'no',
+            'predicted': 'yes',
+            'date':self.date,
+        }
         self._update_date()
-        self._upload_file(container_name = self.cv_pic_container, file_path=cv_pic_path)
-        self._upload_file(container_name = self.txt_container, file_path=txt_path)
-        self._upload_file(container_name = self.predict_pic_container, file_path=predict_pic_path)
+        self._upload_file(container_name = self.cv_pic_container, file_path=cv_pic_path, metadata = metadata)
+        self._upload_file(container_name = self.txt_container, file_path=txt_path, metadata = metadata)
+        self._upload_file(container_name = self.predict_pic_container, file_path=predict_pic_path, metadata = metadata)
 
 
     def list_blobs_in_container(self, container_name):
