cfe69d3486dff92babf47b283f1308db775c54c3
WangCHEN9 <solid.chen9@gmail.com>
2021-06-24 19:12:24 +0200

    add log

--- request_inference_raw.py
+++ request_inference_raw.py
@@ -1,10 +1,12 @@
 import requests
 import os
 import progressbar
+import time
 
 URL = 'http://localhost:8000/EOL/'
 
 def inference_wav_raw(file_path, url = URL):
+    t1 = time.time()
     files = {'wav': open(file_path, 'rb')}
     try:
         r = requests.post(url, files=files)
@@ -16,26 +18,36 @@ def inference_wav_raw(file_path, url = URL):
     except requests.exceptions.RequestException as e:
         # catastrophic error. bail.
         raise SystemExit(e)
-    return r.json()
+    t2 = time.time()
+    return (r.json(), (t2 - t1))
 
-def process_one_wav_folder(wav_folder):
+
+def process_one_wav_folder(wav_folder, log_file_path):
     file_lists = os.listdir(wav_folder)
     print(f'processing {len(file_lists)} wav files..')
 
-    widgets = ['request to API running : ', progressbar.AnimatedMarker(), progressbar.Percentage(), progressbar.Bar(), progressbar.ETA()] 
-    bar = progressbar.ProgressBar(widgets=widgets, maxval=len(file_lists)).start()
+    # widgets = ['request to API running : ', progressbar.AnimatedMarker(), progressbar.Percentage(), progressbar.Bar(), progressbar.ETA()] 
+    # bar = progressbar.ProgressBar(widgets=widgets, maxval=len(file_lists)).start()
 
     for count, file_name in enumerate(file_lists):
         file_path = os.path.join(wav_folder, file_name)
         
-        response = inference_wav_raw(file_path)
-        bar.update(count)
-        print(f'{count} response from faursound-api = {response}')
+        response, time_spend = inference_wav_raw(file_path)
+        # bar.update(count)
+        print(f'number:{count} response from faursound-api = {response}')
+        update_log_file(log_file_path, str(round(time_spend,3)))
+
+
+def update_log_file(log_file, infor:str):
+    with open(log_file, "a") as f:
+        f.write(infor) 
+        f.write("\n")
 
 
 if __name__ == "__main__":
     wav_folder = r'D:\Github\FaurSound\data\01 fev\Waves'
+    log_file = r'./api_log.txt'
     all_sub_folders = [ os.path.join(wav_folder,name) for name in os.listdir(wav_folder) if os.path.isdir(os.path.join(wav_folder, name)) ]
 
     for folder in all_sub_folders:
-        process_one_wav_folder(folder)
+        process_one_wav_folder(folder, log_file)
